d40dc386c6e43a90988b6945e4e98572021e9012
911_Adam_Paul <72084877+pauladam2001@users.noreply.github.com>
2022-03-14 13:54:14 +0200

    Added lab1

---
+++ Homework1/Homework1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Homework1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAgents = new System.Windows.Forms.DataGridView();
+            this.dgvPlayers = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAgents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAgents
+            // 
+            this.dgvAgents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAgents.Location = new System.Drawing.Point(78, 49);
+            this.dgvAgents.Name = "dgvAgents";
+            this.dgvAgents.RowHeadersWidth = 51;
+            this.dgvAgents.RowTemplate.Height = 24;
+            this.dgvAgents.Size = new System.Drawing.Size(620, 137);
+            this.dgvAgents.TabIndex = 0;
+            // 
+            // dgvPlayers
+            // 
+            this.dgvPlayers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPlayers.Location = new System.Drawing.Point(78, 300);
+            this.dgvPlayers.Name = "dgvPlayers";
+            this.dgvPlayers.RowHeadersWidth = 51;
+            this.dgvPlayers.RowTemplate.Height = 24;
+            this.dgvPlayers.Size = new System.Drawing.Size(769, 150);
+            this.dgvPlayers.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Agents";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Basketball Players";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(809, 127);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(226, 75);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update Databse";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 511);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvPlayers);
+            this.Controls.Add(this.dgvAgents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAgents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPlayers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAgents;
+        private System.Windows.Forms.DataGridView dgvPlayers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ Homework1/Homework1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework1
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection dbConn;
+        SqlDataAdapter daAgents, daPlayers;
+        DataSet ds;
+        BindingSource bsAgents, bsPlayers;
+        SqlCommandBuilder cbPlayers;            // we need it for SqlDataAdapter Update command
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                daPlayers.Update(ds, "BasketballPlayer");
+                MessageBox.Show("Changes were saved successfully!");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbConn = new SqlConnection("Data Source = DELLG5-5587-ADA; Initial Catalog = BasketballLeagueDB; Integrated Security = True;");
+
+            ds = new DataSet();
+
+            daAgents = new SqlDataAdapter("SELECT * FROM Agent", dbConn);
+            daPlayers = new SqlDataAdapter("SELECT * FROM BasketballPlayer", dbConn);
+
+            cbPlayers = new SqlCommandBuilder(daPlayers);
+
+            daAgents.Fill(ds, "Agent");
+            daPlayers.Fill(ds, "BasketballPlayer");
+
+            DataRelation dr = new DataRelation("FK_Players_Agents", ds.Tables["Agent"].Columns["AgentID"], ds.Tables["BasketballPlayer"].Columns["AgentID"]);
+
+            ds.Relations.Add(dr);
+
+            bsAgents = new BindingSource();
+            bsAgents.DataSource = ds;
+            bsAgents.DataMember = "Agent";
+
+            bsPlayers = new BindingSource();
+            bsPlayers.DataSource = bsAgents;
+            bsPlayers.DataMember = "FK_Players_Agents";
+
+            dgvAgents.DataSource = bsAgents;
+            dgvPlayers.DataSource = bsPlayers;
+        }
+    }
+}
