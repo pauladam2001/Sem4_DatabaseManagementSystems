861aa732a47be1d0d33fa11586ac4aadcce0428f
911_Adam_Paul <72084877+pauladam2001@users.noreply.github.com>
2022-03-30 11:28:46 +0300

    Added lab2

---
+++ Homework2/Homework1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Homework1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvParent = new System.Windows.Forms.DataGridView();
+            this.dgvChild = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvParent
+            // 
+            this.dgvParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvParent.Location = new System.Drawing.Point(78, 49);
+            this.dgvParent.Name = "dgvParent";
+            this.dgvParent.RowHeadersWidth = 51;
+            this.dgvParent.RowTemplate.Height = 24;
+            this.dgvParent.Size = new System.Drawing.Size(620, 137);
+            this.dgvParent.TabIndex = 0;
+            // 
+            // dgvChild
+            // 
+            this.dgvChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChild.Location = new System.Drawing.Point(78, 300);
+            this.dgvChild.Name = "dgvChild";
+            this.dgvChild.RowHeadersWidth = 51;
+            this.dgvChild.RowTemplate.Height = 24;
+            this.dgvChild.Size = new System.Drawing.Size(769, 150);
+            this.dgvChild.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Parent Table";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Child Table";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(809, 127);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(226, 75);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update Database";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 511);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvChild);
+            this.Controls.Add(this.dgvParent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChild)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvParent;
+        private System.Windows.Forms.DataGridView dgvChild;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ Homework2/Homework1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Homework1
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection dbConn;
+        SqlDataAdapter daParent, daChild;
+        DataSet ds;
+        BindingSource bsParent, bsChild;
+        SqlCommandBuilder cbChild;            // we need it for SqlDataAdapter Update command
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                daChild.Update(ds, getChildTable());
+                MessageBox.Show("Changes were saved successfully!");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string getConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["connString"].ConnectionString.ToString();
+        }
+
+        private string getPKName()
+        {
+            return ConfigurationManager.AppSettings["parentTablePK"];
+        }
+
+        private string getFKName()
+        {
+            return ConfigurationManager.AppSettings["childTableFK"];
+        }
+
+        private string getParentTable()
+        {
+            return ConfigurationManager.AppSettings["parentTable"];
+        }
+
+        private string getChildTable()
+        {
+            return ConfigurationManager.AppSettings["childTable"];
+        }
+
+        private string getParentQuery()
+        {
+            return ConfigurationManager.AppSettings["parentTableQuery"];
+        }
+
+        private string getChildQuery()
+        {
+            return ConfigurationManager.AppSettings["childTableQuery"];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbConn = new SqlConnection(getConnectionString());
+           
+            ds = new DataSet();
+
+            daParent = new SqlDataAdapter(getParentQuery(), dbConn);
+            daChild = new SqlDataAdapter(getChildQuery(), dbConn);
+
+            cbChild = new SqlCommandBuilder(daChild);
+
+            daParent.Fill(ds, getParentTable());
+            daChild.Fill(ds, getChildTable());
+
+            DataRelation dr = new DataRelation("FK_Child_Parent", ds.Tables[getParentTable()].Columns[getPKName()], ds.Tables[getChildTable()].Columns[getFKName()]);
+
+            ds.Relations.Add(dr);
+
+            bsParent = new BindingSource();
+            bsParent.DataSource = ds;
+            bsParent.DataMember = getParentTable();
+
+            bsChild = new BindingSource();
+            bsChild.DataSource = bsParent;
+            bsChild.DataMember = "FK_Child_Parent";
+
+            dgvParent.DataSource = bsParent;
+            dgvChild.DataSource = bsChild;
+        }
+    }
+}
